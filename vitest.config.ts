f785db1a27ce0c0afa39c9e108f6cfcc3a9153bd
exoego <ytatsuno.jp@gmail.com>
2023-10-29 21:46:55 +0900

    add very basic react test

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,35 @@
 import { defineConfig } from 'vitest/config'
+import react from '@vitejs/plugin-react'
+
+import path from "path";
+import fs from "fs";
+
+const base64import = {
+  name: "svg-base64-import",
+  transform(_code: string, id: string) {
+    if (id.endsWith(".svg")) {
+      const svgSrc = path.relative(process.cwd(), id);
+      const svgRaw = fs.readFileSync(svgSrc, { encoding: "utf-8" });
+      const value = `data:image/svg+xml,${encodeURIComponent(svgRaw)}`;
+      return {
+          code: `const value = '${value}'; export default value;`,
+      };
+    }
+  },
+};
 
 export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'happy-dom',
+  },
+  plugins: [
+    react(),
+    base64import,
+  ],
   resolve: {
     alias: {
+      "data-base64:~/": `${__dirname}/`,
       '~': `${__dirname}/`,
       '~/': `${__dirname}/`,
     },
