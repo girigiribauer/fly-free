35c08ad7551a4627e0e38858892d63ced5f8274b
girigiribauer <girigiribauer@gmail.com>
2023-11-08 09:54:54 +0900

    resize and reload #20

--- definitions.ts
+++ definitions.ts
@@ -1,9 +1,12 @@
 export const StorageIdentifier = 'Flyfree'
 export const ContentPageTitle = 'FlyFree - Multiple posts'
 
-export const SelectorTextarea = '[data-testid="tweetTextarea_0"]'
-export const SelectorAttachments = '[data-testid="attachments"]'
-export const SelectorCardWrapper = '[data-testid="card.wrapper"]'
+export const SelectorTextarea =
+  '[data-testid="twc-cc-mask"] ~ div [data-testid="tweetTextarea_0"]'
+export const SelectorAttachments =
+  '[data-testid="twc-cc-mask"] ~ div [data-testid="attachments"]'
+export const SelectorCardWrapper =
+  '[data-testid="twc-cc-mask"] ~ div [data-testid="card.wrapper"]'
 export const SelectorCardDomain = [
   '[data-testid="card.layoutSmall.detail"]>div:first-child',
   '[data-testid="card.layoutLarge.detail"]>div:first-child',
