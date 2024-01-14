e8a20aa38d244d41623aa50f91fe665d0023480d
girigiribauer <girigiribauer@gmail.com>
2024-01-14 14:09:18 +0900

    fix tests #34

--- tests/models/Post.test.ts
+++ tests/models/Post.test.ts
@@ -31,7 +31,7 @@ describe('convertDraft2Post', () => {
         thumbnail: {
           binary: ogpSiteImage,
           mimetype: 'image/png',
-          size: 12379,
+          filesize: 12379,
         },
         title: 'The Open Graph protocol',
         description:
