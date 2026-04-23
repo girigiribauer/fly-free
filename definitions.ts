0d616fb5e706ae5572545ebfe60ce05af6e4e2a0
girigiribauer <girigiribauer@gmail.com>
2026-04-23 09:23:11 +0900

    応急処置

--- definitions.ts
+++ definitions.ts
@@ -1,12 +1,12 @@
 export const StorageIdentifier = 'Flyfree'
 export const ContentPageTitle = 'FlyFree - Social Media Crossposting Tool'
 
-export const SelectorTextarea =
-  '[data-testid="twc-cc-mask"] ~ div [data-testid="tweetTextarea_0"]'
-export const SelectorAttachments =
-  '[data-testid="twc-cc-mask"] ~ div [data-testid="attachments"]'
-export const SelectorLinkcard =
-  '[data-testid="twc-cc-mask"] ~ div [data-testid="card.wrapper"]'
+export const ContainerSelector =
+  ':is([role="dialog"], [role="main"]):has([data-testid="tweetButton"])'
+
+export const SelectorTextarea = `${ContainerSelector} [data-testid="tweetTextarea_0"]`
+export const SelectorAttachments = `${ContainerSelector} [data-testid="attachments"]`
+export const SelectorLinkcard = `${ContainerSelector} [data-testid="card.wrapper"]`
 export const SelectorDroppedImage = [
   'img[src^="blob:https://twitter.com/"]',
   'img[src^="blob:https://x.com/"]',
